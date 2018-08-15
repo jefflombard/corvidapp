86c2d6f3d3660b052eb7cc02e2167839f0cf71e6
Jeff Lombard <jefflombardjr@gmail.com>
2018-08-14 21:28:54 -0400

    router

--- App.js
+++ App.js
@@ -1,18 +1,36 @@
-import React from 'react';
-import { Scene, Router, Stack } from 'react-native-router-flux';
+import React, {Component} from 'react';
+import { Scene, Router } from 'react-native-router-flux';
 import { connect } from 'react-redux';
 import { View } from 'react-native';
 
 import LoginScene from './scenes/Login';
 
-const App = () => (
-  <View>
-    <Router>
-      <Scene key="root">
-        <Scene key="login" component={LoginScene} title="Login"/>
-      </Scene>
-    </Router>
-  </View>
-);
+const renderRouter = (showRouter) => {
+  if (showRouter) {
+    return (
+      <Router>
+        <Scene key="root">
+          <Scene key="login" component={LoginScene} title="Login"/>
+        </Scene>
+      </Router>
+    );
+  }
 
-export default App;
+  return <LoginScene />;
+};
+
+class App extends Component {
+  render(){
+    console.log(this.props,this.state,this.dispatch)
+    return (
+    <View>
+      {renderRouter(true)}
+    </View>
+  );
+  }
+}
+
+
+const mapStateToProps = (state) => ({ auth: state.auth })
+
+export default connect(mapStateToProps)(App);
