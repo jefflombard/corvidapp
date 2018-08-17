9469b9765422a11c45f14bb6da3d565e9580c65a
Jeff Lombard <jefflombardjr@gmail.com>
2018-08-17 17:47:01 -0400

    setup dispatch

--- reducers/auth.js
+++ reducers/auth.js
@@ -1,25 +1,32 @@
-const initialState = {};
+const initialState = {
+  signUp: false,
+};
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case 'TOGGLE_SIGN_UP':
+      return {
+        ...state,
+        signUp: !state.signUp,
+      };
     case 'AUTH_STATUS_CHANGED':
       return {
         ...state,
-          authorized: action.payload
-      }
+        authorized: action.payload,
+      };
     case 'EMAIL_CHANGED':
       return {
         ...state,
         auth: {
-          email: action.payload
-        }
+          email: action.payload,
+        },
       };
     case 'PASSWORD_CHANGED':
       return {
         ...state,
         auth: {
-          password: action.payload
-        }
+          password: action.payload,
+        },
       };
     default:
       return state;
--- scenes/Login/index.js
+++ scenes/Login/index.js
@@ -1,27 +1,52 @@
 import React, { Component } from 'react';
-import { ImageBackground, StyleSheet, Text } from 'react-native';
+import {
+  ImageBackground, StyleSheet, View, Button,
+} from 'react-native';
+import { connect } from 'react-redux';
 
 import LogoContainer from './LogoContainer';
-
-class LoginScene extends Component {
-  render() {
-    return (
-      <ImageBackground
-        style={styles.background}
-        source={require('../../assets/bg.png')}
-        >
-        <LogoContainer />
-      </ImageBackground>
-    );
-  }
-}
 
 const styles = StyleSheet.create({
   background: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+  },
+  switcherContainer: {
+    flexDirection: 'row',
+    marginTop: 55,
+    width: 223,
+    justifyContent: 'space-between',
   },
 });
 
-export default LoginScene;
+class LoginScene extends Component {
+  onTogglePress() {
+    const { dispatch } = this.props;
+    dispatch(
+      {
+        type: 'TOGGLE_SIGN_UP',
+      },
+    );
+  }
+
+  render() {
+    const { background, switcherContainer } = styles;
+    return (
+      <ImageBackground
+        style={background}
+        source={require('../../assets/bg.png')} // eslint-disable-line global-require
+      >
+        <LogoContainer />
+        <View style={switcherContainer}>
+          <Button title="Sign In" color="#fff" onPress={this.onTogglePress.bind(this)}/>
+          <Button title="Sign Up" color="#fff" onPress={this.onTogglePress.bind(this)}/>
+        </View>
+      </ImageBackground>
+    );
+  }
+}
+
+const mapStateToProps = state => ({ auth: state.auth });
+
+export default connect(mapStateToProps)(LoginScene);
