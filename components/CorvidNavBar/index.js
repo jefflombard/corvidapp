03555c6700ddfc0ffaeae3eb5c4a044f97275b07
Jeff Lombard <jefflombardjr@gmail.com>
2018-08-23 10:51:26 -0400

    nav bar markup complete

--- components/CorvidNavBar/index.js
+++ components/CorvidNavBar/index.js
@@ -1,24 +1,63 @@
-import { Image, StyleSheet, Text, TouchableOpacity, View
+import {
+  ImageBackground,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
 } from 'react-native';
 import React, { Component } from 'react';
 import { Actions } from 'react-native-router-flux';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import * as actionCreators from '../../actionCreators';
+
 
 const styles = StyleSheet.create({
   container: {
     height: 64,
     position: 'absolute',
     width: '100%',
-    backgroundColor: "#000",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+    paddingBottom: 12,
+  },
+  title: {
+    fontSize: 17,
+    color: '#fff',
+  },
+  credits: {
+    marginRight: 10,
+    flexDirection: 'row',
+  },
+  creditText: {
+    marginLeft: 10,
+    color: '#fff',
+  },
+  settings: {
+    marginLeft: 10,
   },
 });
 
-export default class CorvidNavBar extends Component {
-  render(){
+const CorvidNavBar = props => (
+  <ImageBackground
+    style={styles.container}
+    source={require('./navbg.png')} // eslint-disable-line global-require
+  >
+    <TouchableOpacity style={styles.settings}>
+      <Image source={require('./gear.png')} />
+    </TouchableOpacity>
+    <Text style={styles.title}>{props.title}</Text>
+    <TouchableOpacity style={styles.credits}>
+      <Image source={require('./envelope.png')} />
+      <Text style={styles.creditText}>21</Text>
+    </TouchableOpacity>
+  </ImageBackground>
+);
 
-    return (
-      <View style={styles.container}>
-        <Text>{this.props.title}</Text>
-      </View>
-    );
-  }
-}
+const mapStateToProps = state => ({ ui: state.ui });
+const mapDispatchToProps = dispatch => ({ actions: bindActionCreators(actionCreators, dispatch) });
+
+export default connect(mapStateToProps, mapDispatchToProps)(CorvidNavBar);
