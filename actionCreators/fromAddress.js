427d0a5e24bd28c0539415424ebaadb38ce3b7b4
Jeff Lombard <jefflombardjr@gmail.com>
2018-09-05 08:21:24 -0400

    finished set from address

--- actionCreators/fromAddress.js
+++ actionCreators/fromAddress.js
@@ -1,55 +1,69 @@
 import { Actions } from 'react-native-router-flux';
+import firebase from 'react-native-firebase';
+import { AlertIOS } from 'react-native';
 
 export const saveFromAddress = () => (dispatch, getState) => {
   // save logic
+  const { fromAddress, auth } = getState();
+  const db = firebase.firestore();
+  const userId = auth.user.uid;
 
-  Actions.postcards();
-};
-
-export const fromUpdateName = name => (dispatch, getState) => {
-  // save logic
+  // Set Loading
   dispatch({
-    type: 'FROM_ADDRESS_UPDATE_NAME',
-    payload: name,
+    type: 'LOADING',
+    payload: true,
   });
+
+  db.collection('users').doc(userId).set({
+    fromAddress,
+  })
+    .then(() => {
+      // Remove Loading
+      dispatch({
+        type: 'LOADING',
+        payload: false,
+      });
+      // Navigate back to postcard
+      Actions.postcards();
+    })
+    .catch((err) => {
+      // Remove Loading
+      dispatch({
+        type: 'LOADING',
+        payload: false,
+      });
+
+      // Alert Error
+      AlertIOS.alert(err);
+    });
 };
 
-export const fromUpdateAddress = address => (dispatch, getState) => {
-  // save logic
-  dispatch({
-    type: 'FROM_ADDRESS_UPDATE_ADDRESS',
-    payload: address,
-  });
-};
+export const fromUpdateName = name => ({
+  type: 'FROM_ADDRESS_UPDATE_NAME',
+  payload: name,
+});
 
-export const fromUpdateAddress2 = address2 => (dispatch, getState) => {
-  // save logic
-  dispatch({
-    type: 'FROM_ADDRESS_UPDATE_ADDRESS2',
-    payload: address2,
-  });
-};
+export const fromUpdateAddress = address => ({
+  type: 'FROM_ADDRESS_UPDATE_ADDRESS',
+  payload: address,
+});
 
-export const fromUpdateCity = city => (dispatch, getState) => {
-  // save logic
-  dispatch({
-    type: 'FROM_ADDRESS_UPDATE_CITY',
-    payload: city,
-  });
-};
+export const fromUpdateAddress2 = address2 => ({
+  type: 'FROM_ADDRESS_UPDATE_ADDRESS2',
+  payload: address2,
+});
 
-export const fromUpdateState = state => (dispatch, getState) => {
-  // save logic
-  dispatch({
-    type: 'FROM_ADDRESS_UPDATE_STATE',
-    payload: state,
-  });
-};
+export const fromUpdateCity = city => ({
+  type: 'FROM_ADDRESS_UPDATE_CITY',
+  payload: city,
+});
 
-export const fromUpdateZip = zip => (dispatch, getState) => {
-  // save logic
-  dispatch({
-    type: 'FROM_ADDRESS_UPDATE_ZIP',
-    payload: zip,
-  });
-};
+export const fromUpdateState = state => ({
+  type: 'FROM_ADDRESS_UPDATE_STATE',
+  payload: state,
+});
+
+export const fromUpdateZip = zip => ({
+  type: 'FROM_ADDRESS_UPDATE_ZIP',
+  payload: zip,
+});
