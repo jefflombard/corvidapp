bc6b510dbeefa9e5644091e47321ed5353227230
Jeff Lombard <jefflombardjr@gmail.com>
2018-09-13 22:05:53 -0400

    fixed info.plist

--- android/app/src/main/java/com/getcorvid/corvid/MainApplication.java
+++ android/app/src/main/java/com/getcorvid/corvid/MainApplication.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -45,6 +46,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.asList(
         new MainReactPackage(),
+            new RNCameraPackage(),
             new RNFirebasePackage(),
         new RNFirebasePackage(),
         // add/remove these packages as appropriate
